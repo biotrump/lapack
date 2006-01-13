235121746d87210b69cdaa8db820b50fe94b4ab1
julie <USER@DOMAIN>
2006-01-13 18:48:40 +0000

    Inserted relevant (input) and (input/output) statements into the argument descriptions

--- SRC/csymv.f
+++ SRC/csymv.f
@@ -27,7 +27,7 @@
 *  Arguments
 *  ==========
 *
-*  UPLO   - CHARACTER*1
+*  UPLO   - (input) CHARACTER*1
 *           On entry, UPLO specifies whether the upper or lower
 *           triangular part of the array A is to be referenced as
 *           follows:
@@ -40,16 +40,16 @@
 *
 *           Unchanged on exit.
 *
-*  N      - INTEGER
+*  N      - (input) INTEGER
 *           On entry, N specifies the order of the matrix A.
 *           N must be at least zero.
 *           Unchanged on exit.
 *
-*  ALPHA  - COMPLEX
+*  ALPHA  - (input) COMPLEX
 *           On entry, ALPHA specifies the scalar alpha.
 *           Unchanged on exit.
 *
-*  A      - COMPLEX array, dimension ( LDA, N )
+*  A      - (input) COMPLEX array, dimension ( LDA, N )
 *           Before entry, with  UPLO = 'U' or 'u', the leading n by n
 *           upper triangular part of the array A must contain the upper
 *           triangular part of the symmetric matrix and the strictly
@@ -60,35 +60,35 @@
 *           upper triangular part of A is not referenced.
 *           Unchanged on exit.
 *
-*  LDA    - INTEGER
+*  LDA    - (input) INTEGER
 *           On entry, LDA specifies the first dimension of A as declared
 *           in the calling (sub) program. LDA must be at least
 *           max( 1, N ).
 *           Unchanged on exit.
 *
-*  X      - COMPLEX array, dimension at least
+*  X      - (input) COMPLEX array, dimension at least
 *           ( 1 + ( N - 1 )*abs( INCX ) ).
 *           Before entry, the incremented array X must contain the N-
 *           element vector x.
 *           Unchanged on exit.
 *
-*  INCX   - INTEGER
+*  INCX   - (input) INTEGER
 *           On entry, INCX specifies the increment for the elements of
 *           X. INCX must not be zero.
 *           Unchanged on exit.
 *
-*  BETA   - COMPLEX
+*  BETA   - (input) COMPLEX
 *           On entry, BETA specifies the scalar beta. When BETA is
 *           supplied as zero then Y need not be set on input.
 *           Unchanged on exit.
 *
-*  Y      - COMPLEX array, dimension at least
+*  Y      - (input/output) COMPLEX array, dimension at least
 *           ( 1 + ( N - 1 )*abs( INCY ) ).
 *           Before entry, the incremented array Y must contain the n
 *           element vector y. On exit, Y is overwritten by the updated
 *           vector y.
 *
-*  INCY   - INTEGER
+*  INCY   - (input) INTEGER
 *           On entry, INCY specifies the increment for the elements of
 *           Y. INCY must not be zero.
 *           Unchanged on exit.
