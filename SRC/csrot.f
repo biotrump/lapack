7fac13d5485be2be02384a94029c9d221e968ada
julie <USER@DOMAIN>
2006-01-13 18:52:02 +0000

    Inserted descriptions of the arguments

--- SRC/csrot.f
+++ SRC/csrot.f
@@ -1,11 +1,5 @@
       SUBROUTINE CSROT( N, CX, INCX, CY, INCY, C, S )
 *
-*     applies a plane rotation, where the cos and sin (c and s) are real
-*     and the vectors cx and cy are complex.
-*     jack dongarra, linpack, 3/11/78.
-*
-*  =====================================================================
-*
 *     .. Scalar Arguments ..
       INTEGER           INCX, INCY, N
       REAL              C, S
@@ -13,6 +7,54 @@
 *     .. Array Arguments ..
       COMPLEX           CX( * ), CY( * )
 *     ..
+*
+*  Purpose
+*  =======
+*
+*  Applies a plane rotation, where the cos and sin (c and s) are real
+*  and the vectors cx and cy are complex.
+*  jack dongarra, linpack, 3/11/78.
+*
+*  Arguments
+*  ==========
+*
+*  N      - (input) INTEGER
+*           On entry, N specifies the order of the vectors cx and cy.
+*           N must be at least zero.
+*           Unchanged on exit.
+*
+*  CX     - (input) COMPLEX array, dimension at least
+*           ( 1 + ( N - 1 )*abs( INCX ) ).
+*           Before entry, the incremented array CX must contain the n
+*           element vector cx. On exit, CX is overwritten by the updated
+*           vector cx.
+*
+*  INCX   - (input) INTEGER
+*           On entry, INCX specifies the increment for the elements of
+*           CX. INCX must not be zero.
+*           Unchanged on exit.
+*
+*  CY     - (input) COMPLEX array, dimension at least
+*           ( 1 + ( N - 1 )*abs( INCY ) ).
+*           Before entry, the incremented array CY must contain the n
+*           element vector cy. On exit, CY is overwritten by the updated
+*           vector cy.
+*
+*  INCY   - (input) INTEGER
+*           On entry, INCY specifies the increment for the elements of
+*           CY. INCY must not be zero.
+*           Unchanged on exit.
+*
+*  C      - (input) REAL
+*           On entry, C specifies the cosine, cos.
+*           Unchanged on exit.
+*
+*  S      - (input) REAL
+*           On entry, S specifies the sine, sin.
+*           Unchanged on exit.
+*
+*  =====================================================================
+*
 *     .. Local Scalars ..
       INTEGER           I, IX, IY
       COMPLEX           CTEMP
