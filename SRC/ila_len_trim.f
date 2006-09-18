111dc33ec93e9480f3e8376105ec3586d53afc24
julie <USER@DOMAIN>
2006-09-18 21:22:42 +0000

    Use the nag tools (nag_decs and nag_polish) to polish the new routine: ila_len_trim

--- SRC/ila_len_trim.f
+++ SRC/ila_len_trim.f
@@ -1,44 +1,43 @@
       INTEGER FUNCTION ILA_LEN_TRIM(SUBNAM)
-!
-!  -- LAPACK auxiliary routine (version 3.0) --
-!     Univ. of Tennessee, Univ. of California Berkeley, NAG Ltd.,
-!     Courant Institute, Argonne National Lab, and Rice University
-!     28 August, 2006
-!
-!     .. Scalar Arguments ..
+C
+C  -- LAPACK auxiliary routine (version 3.0) --
+C     Univ. of Tennessee, Univ. of California Berkeley, NAG Ltd.,
+C     Courant Institute, Argonne National Lab, and Rice University
+C     28 August, 2006
+C
+C     .. Scalar Arguments ..
       CHARACTER*(*) SUBNAM
-!     ..
-!
-!  Purpose
-!  =======
-!
-!  ILA_LEN_TRIM is called from testing and timing routines to remove
-!  trailing spaces from its argument.  It is included in the library
-!  for possible use within a user's XERBLA error-handing routine.
-!
-!  Arguments
-!  =========
-!
-!  SUBNAM  (input) CHARACTER*(*)
-!          Provides the string.
-!
-!  RETURN VALUE:  INTEGER
-!          = N > 0 : The location of the last non-blank.
-!          = 0     : The entire string is blank.
-!
-!     .. Local Scalars ..
+C     ..
+C
+C  Purpose
+C  =======
+C
+C  ILA_LEN_TRIM is called from testing and timing routines to remove
+C  trailing spaces from its argument.  It is included in the library
+C  for possible use within a user's XERBLA error-handing routine.
+C
+C  Arguments
+C  =========
+C
+C  SUBNAM  (input) CHARACTER*(*)
+C          Provides the string.
+C
+C  RETURN VALUE:  INTEGER
+C          = N > 0 : The location of the last non-blank.
+C          = 0     : The entire string is blank.
+C
+C     .. Local Scalars ..
       INTEGER I
-!     ..
-!     .. Intrinsic Functions ..
+C     ..
+C     .. Intrinsic Functions ..
       INTRINSIC LEN
-!     ..
-!     .. Executable Statements ..
+C     ..
 
-      DO I=LEN(SUBNAM), 1, -1
-         IF (SUBNAM(I:I) .NE. ' ') THEN
-            ILA_LEN_TRIM = I
-            RETURN
-         END IF
+      DO I = LEN(SUBNAM),1,-1
+          IF (SUBNAM(I:I).NE.' ') THEN
+              ILA_LEN_TRIM = I
+              RETURN
+          END IF
       END DO
       ILA_LEN_TRIM = 0
       END
