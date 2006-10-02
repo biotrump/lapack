153bb7e4f6eec6e50cf71c040ea4a26a06339a7e
yozo <USER@DOMAIN>
2006-10-02 10:10:42 +0000

    XBLAS: Add the option of using XERBLA in BLAS_error.c
    If CONFIG_USE_XERBLA is set, BLAS_error will call XERBLA
    through XERBLA_ARRAY.  The build system is not set up to
    link the test code with a Fortran library, however.
    
    Original author: Jason Riedy <ejr@cs.berkeley.edu>

--- XBLAS/src/common/BLAS_error.c
+++ XBLAS/src/common/BLAS_error.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 #include "blas_extended.h"
+#include "f2c-bridge.h"
+
+#if defined(CONFIG_USE_XERBLA)
+#define xerbla_array FC_FUNC_(xerbla_array, XERBLA_ARRAY)
+extern void xerbla_array(const char *, const int *, const int *);
+#endif
 
 void BLAS_error(const char *rname, int iflag, int ival, char *form, ...)
 /*
@@ -16,18 +23,28 @@ void BLAS_error(const char *rname, int iflag, int ival, char *form, ...)
  * ival      (input) the value of parameter number -IFLAG.
  */
 {
-  va_list argptr;
-
-  va_start(argptr, form);
-  fprintf(stderr, "Error #%d from routine %s:\n", iflag, rname);
-  if (form)
-    vfprintf(stderr, form, argptr);
-  else if (iflag < 0)
-    fprintf(stderr,
-            "  Parameter number %d to routine %s had the illegal value %d\n",
-            -iflag, rname, ival);
-  else
-    fprintf(stderr, "  Unknown error code %d from routine %s\n",
-            iflag, rname);
-  exit(iflag);
+#if !defined(CONFIG_USE_XERBLA)
+  {
+    va_list argptr;
+    va_start(argptr, form);
+    fprintf(stderr, "Error #%d from routine %s:\n", iflag, rname);
+    if (form)
+      vfprintf(stderr, form, argptr);
+    else if (iflag < 0)
+      fprintf(stderr,
+	      "  Parameter number %d to routine %s had the illegal value %d\n",
+	      -iflag, rname, ival);
+    else
+      fprintf(stderr, "  Unknown error code %d from routine %s\n",
+	      iflag, rname);
+    exit(iflag);
+  }
+#else
+  {
+    int ln, argno;
+    ln = strlen(rname);
+    argno = -iflag;
+    xerbla_array(rname, &ln, &argno);
+  }
+#endif
 }
