25e89a638c528a8d5076c92c6ac0e8f2085307f5
jason <USER@DOMAIN>
2006-10-28 04:30:09 +0000

    Add blocking to LANEG
    Some architectures propagate NaNs very slowly (e.g. some
    Pentium 4s).  To prevent driving off a performance cliff in
    "common exceptional cases", this splits the xLANEG loop into
    blocks.
    
    The block length is an internal parameter and is not handled
    by ILAENV.  It's really not a tuning parameter but rather a
    level of performance paranoia.  Playing with the block size
    will have little impact on typical performance, and platform
    optimizers likely would write a specific xLANEG loop.
    
    Signed-off-by: Jason Riedy <ejr@cs.berkeley.edu>

--- SRC/dlaneg.f
+++ SRC/dlaneg.f
@@ -72,69 +72,87 @@
 *     .. Parameters ..
       DOUBLE PRECISION   ZERO
       PARAMETER        ( ZERO = 0.0D0 )
+*     Some architectures propagate Infinities and NaNs very slowly, so
+*     the code computes counts in BLKLEN chunks.  Then a NaN can
+*     propagate at most BLKLEN columns before being detected.  This is
+*     not a general tuning parameter; it needs only to be just large
+*     enough that the overhead is tiny in common cases.
+      INTEGER BLKLEN
+      PARAMETER ( BLKLEN = 128 )
 *     ..
 *     .. Local Scalars ..
-      INTEGER            J, NEG1, NEG2, NEGCNT
-      DOUBLE PRECISION   DMINUS, DPLUS, GAMMA, P, T, TMP
+      INTEGER            BJ, J, NEG1, NEG2, NEGCNT
+      DOUBLE PRECISION   BSAV, DMINUS, DPLUS, GAMMA, P, T, TMP
       LOGICAL SAWNAN
+*     ..
+*     .. Intrinsic Functions ..
+      INTRINSIC MIN, MAX
 *     ..
 *     .. External Functions ..
       LOGICAL DISNAN
       EXTERNAL DISNAN
+*     ..
 *     .. Executable Statements ..
 
       NEGCNT = 0
 
 *     I) upper part: L D L^T - SIGMA I = L+ D+ L+^T
-      NEG1 = 0
       T = -SIGMA
-      DO 21 J = 1, R - 1
-         DPLUS = D( J ) + T
-         IF( DPLUS.LT.ZERO ) NEG1 = NEG1 + 1
-         TMP = T / DPLUS
-         T = TMP * LLD( J ) - SIGMA
- 21   CONTINUE
-      SAWNAN = DISNAN( S )
+      DO 210 BJ = 1, R-1, BLKLEN
+         NEG1 = 0
+         BSAV = T
+         DO 21 J = BJ, MIN(BJ+BLKLEN-1, R-1)
+            DPLUS = D( J ) + T
+            IF( DPLUS.LT.ZERO ) NEG1 = NEG1 + 1
+            TMP = T / DPLUS
+            T = TMP * LLD( J ) - SIGMA
+ 21      CONTINUE
+         SAWNAN = DISNAN( S )
 *     Run a slower version of the above loop if a NaN is detected.
 *     A NaN should occur only with a zero pivot after an infinite
 *     pivot.  In that case, substituting 1 for T/DPLUS is the
 *     correct limit.
-      IF( SAWNAN ) THEN
-         NEG1 = 0
-         T = -SIGMA
-         DO 22 J = 1, R - 1
-            DPLUS = D( J ) + T
-            IF( DPLUS.LT.ZERO ) NEG1 = NEG1 + 1
-            TMP = T / DPLUS
-            IF (DISNAN(TMP)) TMP = ONE
-            T = TMP * LLD(J) - SIGMA
- 22      CONTINUE
-      END IF
-      NEGCNT = NEGCNT + NEG1
+         IF( SAWNAN ) THEN
+            NEG1 = 0
+            T = BSAV
+            DO 22 J = BJ, MIN(BJ+BLKLEN-1, R-1)
+               DPLUS = D( J ) + T
+               IF( DPLUS.LT.ZERO ) NEG1 = NEG1 + 1
+               TMP = T / DPLUS
+               IF (DISNAN(TMP)) TMP = ONE
+               T = TMP * LLD(J) - SIGMA
+ 22         CONTINUE
+         END IF
+         NEGCNT = NEGCNT + NEG1
+ 210  CONTINUE
 *
 *     II) lower part: L D L^T - SIGMA I = U- D- U-^T
-      NEG2 = 0
       P = D( N ) - SIGMA
-      DO 23 J = N - 1, R, -1
-         DMINUS = LLD( J ) + P
-         IF( DMINUS.LT.ZERO ) NEG2 = NEG2 + 1
-         TMP = P / DMINUS
-         P = TMP * D( J ) - SIGMA
- 23   CONTINUE
-      SAWNAN = DISNAN( P )
-*     As above, run a slower version that substitutes 1 for Inf/Inf.      
-      IF( SAWNAN ) THEN
+      DO 230 BJ = N-1, R, -BLKLEN
          NEG2 = 0
-         P = D( N ) - SIGMA
-         DO 24 J = N - 1, R, -1
+         BSAV = P
+         DO 23 J = BJ, MAX(BJ-BLKLEN+1, R), -1
             DMINUS = LLD( J ) + P
             IF( DMINUS.LT.ZERO ) NEG2 = NEG2 + 1
             TMP = P / DMINUS
-            IF (DISNAN(TMP)) TMP = ONE
-            P = TMP * D(J) - SIGMA
- 24      CONTINUE
-      END IF
-      NEGCNT = NEGCNT + NEG2
+            P = TMP * D( J ) - SIGMA
+ 23      CONTINUE
+         SAWNAN = DISNAN( P )
+*     As above, run a slower version that substitutes 1 for Inf/Inf.
+*
+         IF( SAWNAN ) THEN
+            NEG2 = 0
+            P = BSAV
+            DO 24 J = BJ, MAX(BJ-BLKLEN+1, R), -1
+               DMINUS = LLD( J ) + P
+               IF( DMINUS.LT.ZERO ) NEG2 = NEG2 + 1
+               TMP = P / DMINUS
+               IF (DISNAN(TMP)) TMP = ONE
+               P = TMP * D(J) - SIGMA
+ 24         CONTINUE
+         END IF
+         NEGCNT = NEGCNT + NEG2
+ 230  CONTINUE
 *
 *     III) Twist index
 *       T was shifted by SIGMA initially.
