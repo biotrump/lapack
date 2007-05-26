6adfb9770e364bcd93350d986a996674e1c83d66
jason <USER@DOMAIN>
2007-05-26 18:15:37 +0000

    Fix parameter TWO in cheevr and dsyevr.
    Reported by Mikhail Wolfson <wolfsonm@mit.edu>.
    
    Signed-off-by: Jason Riedy <ejr@cs.berkeley.edu>

--- SRC/cheevr.f
+++ SRC/cheevr.f
@@ -245,7 +245,7 @@
 *
 *     .. Parameters ..
       REAL               ZERO, ONE, TWO
-      PARAMETER          ( ZERO = 0.0E+0, ONE = 1.0E+0, TWO = 1.0E+0 )
+      PARAMETER          ( ZERO = 0.0E+0, ONE = 1.0E+0, TWO = 2.0E+0 )
 *     ..
 *     .. Local Scalars ..
       LOGICAL            ALLEIG, INDEIG, LOWER, LQUERY, TEST, VALEIG,
--- SRC/dsyevr.f
+++ SRC/dsyevr.f
@@ -228,7 +228,7 @@
 *
 *     .. Parameters ..
       DOUBLE PRECISION   ZERO, ONE, TWO
-      PARAMETER          ( ZERO = 0.0D+0, ONE = 1.0D+0, TWO = 0.0D+0 )
+      PARAMETER          ( ZERO = 0.0D+0, ONE = 1.0D+0, TWO = 2.0D+0 )
 *     ..
 *     .. Local Scalars ..
       LOGICAL            ALLEIG, INDEIG, LOWER, LQUERY, VALEIG, WANTZ,
